53e22d485b838ce8abec49e778b0ad4c9844b6b8
Codingsucks123 <winnu.p1819@gmail.com>
2024-12-15 05:42:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //Treat all lines as newer version
+
+//alert(3+3)
+
+let name = "winnu"
+let age = 18
+let isLoggedin = false
+
+
+//number==> 2 to the power of 53
+//bigInt
+//string==>'Hello' or "Hello" our wish
+//boolean==> true/false
+//null==> Its a standalone value,cannot be assigned
+//undefined ==>Value is not assigned
+//symbol ==> To define uniqueness
+//Till here all are primitive datatypes
+
+//object==> We'll see later
+
+console.log(typeof null);//op-object
+console.log(typeof undefined)//op-undefined
